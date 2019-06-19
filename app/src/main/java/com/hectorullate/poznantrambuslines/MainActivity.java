888f2024edc13957b04ec447ca2e46f0b03412b5
Héctor Ullate <hector.ullate@gmail.com>
2019-06-19 16:21:06 +0200

    basic ui implemented

--- app/src/main/java/com/hectorullate/poznantrambuslines/MainActivity.java
+++ app/src/main/java/com/hectorullate/poznantrambuslines/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hectorullate.poznantrambuslines;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,8 +17,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.hectorullate.poznantrambuslines.Fragments.MapFragment;
+import com.hectorullate.poznantrambuslines.Fragments.MyStopsFragment;
+import com.hectorullate.poznantrambuslines.Fragments.SearchFragment;
+import com.hectorullate.poznantrambuslines.Fragments.SettingsFragment;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MyStopsFragment.OnFragmentInteractionListener,
+        SearchFragment.OnFragmentInteractionListener,
+        MapFragment.OnFragmentInteractionListener,
+        SettingsFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +35,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -38,6 +43,10 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        loadFragment(new MyStopsFragment());
+        navigationView.getMenu().getItem(0).setChecked(true);
+        getSupportActionBar().setTitle(navigationView.getMenu().getItem(0).getTitle());
     }
 
     @Override
@@ -77,23 +86,31 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_myStops) {
+            loadFragment(new MyStopsFragment());
+        } else if (id == R.id.nav_search) {
+            loadFragment(new SearchFragment());
+        } else if (id == R.id.nav_map) {
+            loadFragment(new MapFragment());
+        } else if (id == R.id.nav_settings) {
+            loadFragment(new SettingsFragment());
         }
+
+        getSupportActionBar().setTitle(item.getTitle());
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void loadFragment(Fragment f){
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragmentContainer, f).commit();
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
